716ca086fa29aeb6cb8162c6d33e7e8a0ceb1585
Andre Luiz <112490162+AndreLFN1@users.noreply.github.com>
2025-12-03 16:51:27 -0300

    atualizações

--- EP03.py
+++ EP03.py
@@ -2,7 +2,7 @@ import math
 import copy
 
 DELTA_T = 0.1
-GRAVIDADE = 2
+g = 2
 
 # ======================================================================
 # FUNÇÕES OBRIGATÓRIAS
@@ -300,17 +300,32 @@ def main():
     # Loop de impressão da matriz em um plano 2d
     imprimeMatriz(matriz_inicial)
 
-    # Variáveis globais
-    pi = math.pi    
     velocidade_inicial = float(input('Digite a velocidade de lançamento inicial (m/s): '))
     theta = grau2Radiano(int(input('Digite o ângulo de lançamento inicial (em graus): ')))
-    g = 2
-    DELTA_T = 0.1
-    V0_x = velocidade_inicial*math.cos(theta)
-    V0_y =  velocidade_inicial*math.sin(theta)
-    
-    velocidade_atualizada = atualizaVelocidade(V0_x,V0_y,DELTA_T)
 
+    Vx = round(velocidade_inicial * math.cos(theta), 2)
+    Vy = round(velocidade_inicial * math.sin(theta), 2)
+    
+    floor_position = 0
+    left_grid = -100
+    right_grid = 100
+
+    x_atualizado = 0
+    y_atualizado = 0
+
+    while left_grid <= x_atualizado <= right_grid and y_atualizado >= floor_position:
+        # Atualiza posição com base no passo discreto
+        xy_posicao = (atualizaPosicao(x_atualizado, y_atualizado, Vx, Vy, dt=DELTA_T))
+        x_atualizado = xy_posicao[0]
+        y_atualizado = xy_posicao[1]
+
+        # Arredondando para desenhar o gráfico
+        x_grafico = round(x_atualizado, 0)
+        y_grafico = round(y_atualizado, 0)
+        # Atualiza velocidade vertical
+        
+        vxvy_atualizado = atualizaVelocidade(Vx, Vy, dt=DELTA_T)
+        Vy = vxvy_atualizado[1]
 
    
 
